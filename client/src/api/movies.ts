6ae415ed5d460517c21b81d1e810c59efb0fc4dc
alsts <astserba@gmail.com>
2024-12-23 21:25:32 +0000

    Url fix

--- client/src/api/movies.ts
+++ client/src/api/movies.ts
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: process.env.NODE_ENV === 'production'
-  ? 'https://cinemate-backend-iz6i.onrender.com/api`'
-  : 'http://localhost:3000/api',
+    ? 'https://cinemate-backend-iz6i.onrender.com/api'
+    : 'http://localhost:3000/api',
 })
 
 export interface Movie {
