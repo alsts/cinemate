5e77b85717977c95421048cd629c893525b3e7f9
alsts <astserba@gmail.com>
2024-12-26 23:36:56 +0000

    Making Telegram full screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import { useEffect } from 'react'
+import WebApp from '@twa-dev/sdk'
 
 function App() {
+  useEffect(() => {
+    WebApp.expand()
+  }, [])
+
   return (
     <div className="App">
       <Routes>
